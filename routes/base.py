5a1fef7bdfbed57df05d71a1e92c4b9336c30903
mahmoudibrahims <mahmoudibrahimabdelfattah48@gmail.com>
2025-03-22 06:17:31 +0200

    update the base

--- routes/base.py
+++ routes/base.py
@@ -1,10 +1,13 @@
 from fastapi import FastAPI, APIRouter
 
-base_router = APIRouter()
+base_router = APIRouter(
+    prefix ="/api/v1",
+    tags =["api_v1"],
+)
 
 @base_router.get('/')
 def welcome():
     return {
-        "massage":"hello world"
+        "massage":"hello mahmoud "
 
     }
