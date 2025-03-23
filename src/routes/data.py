d8ad4c0a400ce8a9341c1eddaf9ba8dc4ac8d378
mahmoudibrahims <mahmoudibrahimabdelfattah48@gmail.com>
2025-03-23 06:25:22 +0200

    update file

--- src/routes/data.py
+++ src/routes/data.py
@@ -3,6 +3,8 @@ from fastapi.responses import JSONResponse
 import os 
 from helpers.config import get_settings ,settings
 from  controllers import DataController ,ProjectController
+import aiofiles
+from models import ResponseSignal
 
 data_router = APIRouter(
     prefix ="/api/v1/data",
@@ -35,6 +37,22 @@ async def upload_data(project_id:str ,file : UploadFile,
 
 
     project_dir_path = ProjectController().get_project_path(project_id=project_id)
+    file_path = os.path.join (
+        project_dir_path,
+        file.filename,
+    )
+
+    async with aiofiles.open(file_path,"wb")as f:
+        while chunk := await file.read(app_settings.FILE_DEFAULT_CHUNK_SIZE):
+            await f.write(chunk)
+
+    return JSONResponse(
+                content ={
+                    "signal" : ResponseSignal.FILE_UPLOADED_Success.value
+                },
+        )
+
+
 
 
 
