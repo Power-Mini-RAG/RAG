6f5b9feec27b0d05c1bd00bba3bd7166db1d6c55
mahmoudibrahims <mahmoudibrahimabdelfattah48@gmail.com>
2025-04-07 08:29:43 +0200

    update file

--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from routes import base, data
+from routes import base, data ,nlp
 from motor.motor_asyncio import AsyncIOMotorClient
 from helpers.config import get_settings
 from Stores.llm.LLMProviderFactory import LLMProviderFactory
@@ -45,3 +45,4 @@ async def shutdown_span():
 
 app.include_router(base.base_router)
 app.include_router(data.data_router)
+app.include_router(nlp.nlp_router)
